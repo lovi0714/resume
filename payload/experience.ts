a32ba602c6f40a361251aeb1b174919b9ba7bd30
이호인 <lovi0714@kyonggi.ac.kr>
2023-06-05 23:22:57 +0900

    20230605
    퇴사반영

--- payload/experience.ts
+++ payload/experience.ts
@@ -6,10 +6,15 @@ const experience: IExperience.Payload = {
   list: [
     {
       title: '와이즈넛',
-      position: '기술지원팀',
+      position: '기술지원 1팀',
       startedAt: '2022-10',
-      descriptions: ['검색엔진 SF-1 유지/관리 업무'],
-      skillKeywords: ['Linux', 'JSP', 'Java', 'ORACLE', 'MYSQL', 'html', 'css', 'javascript'],
+      endedAt: '2023-06',
+      descriptions: [
+        '검색엔진 SF-1 유지/관리 업무',
+        'linux 환경에서 작업 가능할 정도의 기본 지식 습득',
+        '다수의 트러블 슈팅 경험 (80회 이상 건수 처리)',
+      ],
+      skillKeywords: ['Linux', 'JSP', 'Java', 'ORACLE', 'MYSQL', 'javascript'],
     },
     {
       title: '쌍용교육센터',
